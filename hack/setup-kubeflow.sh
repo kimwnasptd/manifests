4427145476f62d2d2def80c06ec9d18d68482f3e
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2022-02-07 22:20:32 +0200

    Fix kustomize
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

--- hack/setup-kubeflow.sh
+++ hack/setup-kubeflow.sh
@@ -15,26 +15,35 @@
 # limitations under the License.
 
 # This shell script is used to setup Katib deployment.
+set -euo pipefail
 
-set -o errexit
-set -o nounset
-set -o pipefail
+TIMEOUT=600s  # 10mins
 
 echo "Creating Kubeflow namespace..."
-#kubectl create namespace kubeflow
+kubectl create namespace kubeflow
 
 echo "Deploying all Kubeflow components..."
-while ! kustomize build example --load_restrictor none | kubectl apply -f - --validate=false; do echo "Retrying to apply resources"; sleep 10; done
+function install_kubeflow {
+    kustomize build example --load_restrictor none | kubectl apply -f - --validate=false
+}
+
+while ! install_kubeflow;
+do
+    echo "Retrying to apply resources"
+    sleep 10
+done
 
 echo "---"
 echo "Waiting for all Kubeflow components to become ready."
-TIMEOUT=600s  # 10mins
 
-echo "Waiting for istio-system/istiod to become ready..."
-kubectl wait --timeout=${TIMEOUT} -n istio-system -l app=istiod --for=condition=Ready pod
+echo "Waiting for Cert Manager pods to become ready..."
+kubectl wait --timeout=${TIMEOUT} -n cert-manager --all --for=condition=Ready pod
 
-echo "Waiting for istio-system/istio-ingressgateway to become ready..."
-kubectl wait --timeout=${TIMEOUT} -n istio-system -l app=istio-ingressgateway --for=condition=Ready pod
+echo "Waiting for istio-system Pods to become ready..."
+kubectl wait --timeout=${TIMEOUT} -n istio-system --all --for=condition=Ready pod
+
+echo "Waiting for knative-serving Pods to become ready..."
+kubectl wait --timeout=${TIMEOUT} -n knative-serving --all --for=condition=Ready pod
 
 echo "Waiting for kubeflow/ml-pipelines to become ready..."
 kubectl wait --timeout=${TIMEOUT} -n kubeflow -l app=ml-pipeline --for=condition=Ready pod
